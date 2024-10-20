08baeb3540a4c4764a1125f1c7fd47a6457cd2af
aneel tripathee <aneeltripathy56@gmail.com>
2024-10-20 16:14:55 +0545

    Ping command

--- app/main.js
+++ app/main.js
@@ -6,7 +6,12 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
-  connection.write("+PONG\r\n");
+  connection.on("data", (data) => {
+    console.log({ data: data.toString() });
+    if (data.toString() === "*1\r\n$4\r\nPING\r\n")
+      connection.write("+PONG\r\n");
+    connection.end();
+  });
 });
 //
 server.listen(6379, "127.0.0.1");
