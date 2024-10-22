902815e034afd074ef5168bdfa98c398ce2c0df7
aneel tripathee <aneeltripathy56@gmail.com>
2024-10-22 13:18:21 +0545

    read config

--- app/main.js
+++ app/main.js
@@ -6,6 +6,7 @@ const server = net.createServer((connection) => {
   // Handle connection
   connection.on("data", (data) => {
     const commands = Buffer.from(data).toString().split("\r\n");
+    console.log(commands);
     if (commands[2] === "SET") {
       connection.write("+OK\r\n"); // Redis protocol for success
       store.set(commands[4], commands[6]);
@@ -32,6 +33,12 @@ const server = net.createServer((connection) => {
       const str = commands[4];
       const l = str.length;
       return connection.write("$" + l + "\r\n" + str + "\r\n");
+    }
+
+    if (commands[2] === "config") {
+      store.set("dir", path);
+      store.set("dbfilename", file);
+      let result = store.get(value);
     }
     if (commands[2] === "PING") connection.write("+PONG\r\n");
   });
