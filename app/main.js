4780a1cc300d6d886b112608755f1065dd43b69a
aneel tripathee <aneeltripathy56@gmail.com>
2024-10-22 20:36:48 +0545

    read config

--- app/main.js
+++ app/main.js
@@ -3,12 +3,16 @@ const fs = require("fs");
 console.log("Logs from your program will appear here!");
 
 const store = new Map();
+const arguments = new Map();
+
 const server = net.createServer((connection) => {
+  arguments.set(process.argv[1], process.argv[2]);
+  arguments.set(process.argv[3], process.argv[4]);
   // Handle connection
   connection.on("data", (data) => {
     const commands = Buffer.from(data).toString().split("\r\n");
-    const [, , dir, path, dbfilename, file] = process.argv;
-    console.log([dir, path, dbfilename, file]);
+    // const [, , dir, path, dbfilename, file] = process.argv;
+    // console.log([dir, path, dbfilename, file]);
     if (commands[2] === "SET") {
       connection.write("+OK\r\n"); // Redis protocol for success
       store.set(commands[4], commands[6]);
@@ -31,28 +35,17 @@ const server = net.createServer((connection) => {
     }
 
     // *2\r\n $5 \r\n ECHO \r\n $3 \r\n hey \r\n
-    if (commands[2] == "ECHO") {
+    if (commands[2] === "ECHO") {
       const str = commands[4];
       const l = str.length;
       return connection.write("$" + l + "\r\n" + str + "\r\n");
     }
+    if(commands[2] === "config" && commands[3] === "GET"){
+      return `*2\r\n$${arguments.size}\r\n${arguments.get(dir)}\r\n${argumnets.get(dbfileName)\r\n}`
+    }
 
-    if (commands[2] === "config") {
-      store.set("dir", path);
-      store.set("dbfilename", file);
-      let result = store.get(value);
-      console.log(result);
-    }
     if (commands[2] === "PING") connection.write("+PONG\r\n");
-    if (commands[1] === "--dir") {
-      store.set(commands[0], commands[2]);
-      const data = fs.readFileSync(
-        path.resolve(path.cwd(), commands[2], commands[4])
-      );
-      connection.write(data);
-      console.log(data);
-    }
   });
 });
-//
+
 server.listen(6379, "127.0.0.1");
