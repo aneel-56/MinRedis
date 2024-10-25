10bbc7c80eaf32af1fa27d45d9d82efa0c6e123c
aneel tripathee <aneeltripathy56@gmail.com>
2024-10-25 15:55:25 +0545

    read multiple keys

--- app/main.js
+++ app/main.js
@@ -94,7 +94,7 @@ const server = net.createServer((connection) => {
       });
       connection.write(response);
 
-      if (commands[3] === "*") {
+      if (commands[2] === "KEYS" && commands[3] === "*") {
         console.log("********");
         let mulRes = "";
         let header = `*${dataStore.size}\r\n`;
